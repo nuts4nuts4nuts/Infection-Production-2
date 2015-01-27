41928bb005a7ba69facb06a7947c4e32503bfedf
nuts4nuts4nuts <nuts4nuts4nuts@gmail.com>
2015-01-27 01:21:59 -0500

    added board, basic piece and got really scrubby movement

---
+++ Infection/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    // 0 = Player1's turn, 1 = Player2's turn, 2+ pause, menus, etc?
+    public int currentState = 0;
+    private GameObject selectedPiece;
+    private Color pieceOldColor;
+
+	// Use this for initialization
+	void Start ()
+    {
+        print("hello world!");
+	}
+
+    public bool SelectPiece(GameObject newPiece)
+    {
+        selectedPiece = newPiece;
+
+        if (selectedPiece)
+        {
+            pieceOldColor = selectedPiece.renderer.material.color;
+
+            //Highlight that joint!
+            selectedPiece.renderer.material.color = Color.blue;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool MovePiece(Vector2 newPos) 
+    {
+        selectedPiece.transform.position = newPos;
+        selectedPiece.renderer.material.color = pieceOldColor;
+        selectedPiece = null;
+
+        return true;
+    }
+
+}
---
+++ Infection/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    private GameManager gameManager; // GameObject responsible for the management of the game
+    private Camera camera;
+    private bool isPieceSelected;
+ 
+	// Use this for initialization
+	void Start()
+    {
+        gameManager = gameObject.GetComponent<GameManager>();
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        isPieceSelected = false;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        PollMouseInput();
+	}
+
+    private void PollMouseInput()
+    {
+        Ray selectionRay;
+        RaycastHit hitInfo;
+        Vector2 tileCoord;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectionRay = camera.ScreenPointToRay(Input.mousePosition);
+
+            //Start that cast!
+            if (Physics.Raycast(selectionRay, out hitInfo))
+            {
+                if (!isPieceSelected)
+                {
+                    if (hitInfo.collider.gameObject.tag == "GamePiece")
+                    {
+                        isPieceSelected = gameManager.SelectPiece(hitInfo.collider.gameObject);
+                    }
+                }
+                else
+                {
+                    if (hitInfo.collider.gameObject.tag == "Tile")
+                    {
+                        Transform colliderTransform = hitInfo.collider.gameObject.transform;
+                        tileCoord = new Vector2(colliderTransform.position.x, colliderTransform.position.y);
+                        isPieceSelected = !gameManager.MovePiece(tileCoord);
+                    }
+                }
+            }
+        }
+    }
+}
